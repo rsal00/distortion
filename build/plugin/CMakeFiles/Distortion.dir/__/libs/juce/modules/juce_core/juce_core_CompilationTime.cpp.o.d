c2ddebdf594b02e9fd5352d96b1df10b2595c646
Ruben Salazar <rsala0108@gmail.com>
2026-09-17 18:16:25 -0700

    update to .gitignore

--- build/plugin/CMakeFiles/Distortion.dir/__/libs/juce/modules/juce_core/juce_core_CompilationTime.cpp.o.d
+++
@@ -1,2 +0,0 @@
-plugin/CMakeFiles/Distortion.dir/__/libs/juce/modules/juce_core/juce_core_CompilationTime.cpp.o: \
-  /Users/rubensalazar/Programming/AUDIO/distortion/libs/juce/modules/juce_core/juce_core_CompilationTime.cpp
