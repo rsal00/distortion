9cbd3a87c2538c90027b581ed1fbd37438be3bbc
Ruben Salazar <rsala0108@gmail.com>
2026-09-17 19:41:57 -0700

    added assets/ directory and background image to plugin

--- plugin/include/afdistort/PluginEditor.h
+++ plugin/include/afdistort/PluginEditor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PluginProcessor.h"
+#include "BinaryData.h"
 
 //==============================================================================
 class DistortionAudioProcessorEditor final : public juce::AudioProcessorEditor
@@ -10,13 +11,19 @@ public:
     ~DistortionAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    // void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     DistortionAudioProcessor& processorRef;
+    juce::ImageComponent background;
+
+    juce::Image bg_img = juce::ImageCache::getFromMemory(
+        assets::background_png, 
+        assets::background_pngSize
+    );
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DistortionAudioProcessorEditor)
 };
--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -6,9 +6,14 @@ DistortionAudioProcessorEditor::DistortionAudioProcessorEditor (DistortionAudioP
     : AudioProcessorEditor (&p), processorRef (p)
 {
     juce::ignoreUnused (processorRef);
+
+    background.setImage(bg_img);
+    background.setImagePlacement(juce::RectanglePlacement::fillDestination);
+    
+    addAndMakeVisible(background);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (570, 240);
 }
 
 DistortionAudioProcessorEditor::~DistortionAudioProcessorEditor()
@@ -16,18 +21,20 @@ DistortionAudioProcessorEditor::~DistortionAudioProcessorEditor()
 }
 
 //==============================================================================
-void DistortionAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+// void DistortionAudioProcessorEditor::paint (juce::Graphics& g)
+// {
+//     // (Our component is opaque, so we must completely fill the background with a solid colour)
+//     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
-}
+//     g.setColour (juce::Colours::white);
+//     g.setFont (15.0f);
+//     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+// }
 
 void DistortionAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    const auto bounds = getLocalBounds();
+    background.setBounds(bounds);
 }
